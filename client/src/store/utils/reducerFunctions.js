724e179e816bc79878894df4ed65b5226d5e58ed
Seun Soetan <sesoetan@gmail.com>
2021-11-08 11:26:59 -0500

    implement comments in code review

--- client/src/store/utils/reducerFunctions.js
+++ client/src/store/utils/reducerFunctions.js
@@ -11,9 +11,10 @@ export const addMessageToStore = (state, payload) => {
     return [newConvo, ...state];
   }
 
-  return state.map((convo) => {
+  return state.map(convo => {
     if (convo.id === message.conversationId) {
-      const newConvo = { ...convo, messages: [...convo.messages, message] };
+      const newConvo = { ...convo };
+      newConvo.messages.push(message);
       newConvo.latestMessageText = message.text;
       return newConvo;
     } else {
@@ -23,7 +24,7 @@ export const addMessageToStore = (state, payload) => {
 };
 
 export const addOnlineUserToStore = (state, id) => {
-  return state.map((convo) => {
+  return state.map(convo => {
     if (convo.otherUser.id === id) {
       const convoCopy = { ...convo };
       convoCopy.otherUser.online = true;
@@ -35,7 +36,7 @@ export const addOnlineUserToStore = (state, id) => {
 };
 
 export const removeOfflineUserFromStore = (state, id) => {
-  return state.map((convo) => {
+  return state.map(convo => {
     if (convo.otherUser.id === id) {
       const convoCopy = { ...convo };
       convoCopy.otherUser.online = false;
@@ -50,12 +51,12 @@ export const addSearchedUsersToStore = (state, users) => {
   const currentUsers = {};
 
   // make table of current users so we can lookup faster
-  state.forEach((convo) => {
+  state.forEach(convo => {
     currentUsers[convo.otherUser.id] = true;
   });
 
   const newState = [...state];
-  users.forEach((user) => {
+  users.forEach(user => {
     // only create a fake convo if we don't already have a convo with this user
     if (!currentUsers[user.id]) {
       let fakeConvo = { otherUser: user, messages: [] };
@@ -67,17 +68,13 @@ export const addSearchedUsersToStore = (state, users) => {
 };
 
 export const addNewConvoToStore = (state, recipientId, message) => {
-  console.log('addNewConvoToStore', state, recipientId, message);
-  return state.map((convo) => {
-    console.log('ccc', convo);
+  return state.map(convo => {
     if (convo.otherUser.id === recipientId) {
-      const newConvo = { 
-        ...convo, 
-        id: message.conversationId, 
-        messages: [...convo.messages, message],
-        latestMessageText: message.text
-      };
-      return newConvo;
+      const convoCopy = { ...convo };
+      convoCopy.id = message.conversationId;
+      convoCopy.latestMessageText = message.text;
+      convoCopy.messages.push(message);
+      return convoCopy;
     } else {
       return convo;
     }
